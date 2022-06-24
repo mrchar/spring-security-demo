c3da9c09dabe257feeb1ea358dbc65e70219dabd
mrchar <mrchar@qq.com>
2022-06-24 15:43:54 +0800

    初始化项目

---
+++ src/test/java/net/mrchar/demo/springsecurity/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mrchar.demo.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
